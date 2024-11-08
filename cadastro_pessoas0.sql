b4a5cd66fc7e43d497d1ea70875c1c2dc0d2758a
Joziane Oliveira <jozioliveira@edu.uniube.br>
2024-11-07 21:33:18 -0300

    Atualiza o dump do banco de dados `cadastro` com as tabelas `pessoas` e `cursos`
    - Criação e configuração do banco de dados `cadastro` com codificação `utf8mb3`.
    - Modificação na estrutura da tabela `pessoas`:
      - Adição da coluna `id` como chave primária com auto incremento.
      - Inclusão de novas colunas: `profissao`, `nascimento` (tipo `date`), `peso` e `altura` (como `decimal`), com nacionalidade padrão `Brasil`.
    - Criação da tabela `cursos`:
      - Inclui campos como `idcurso` (chave primária), `nome` (único), `descricao`, `carga`, `totaulas` e `ano` (padrão 2016).
    - Adição de dados de exemplo para a tabela `pessoas`.
    - Conclusão do dump com configurações e dados atualizados.

---
+++ cadastro_pessoas0.sql
@@ -0,0 +1,59 @@
+CREATE DATABASE  IF NOT EXISTS `cadastro` /*!40100 DEFAULT CHARACTER SET utf8mb3 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `cadastro`;
+-- MySQL dump 10.13  Distrib 8.0.40, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: cadastro
+-- ------------------------------------------------------
+-- Server version	8.0.40
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `pessoas`
+--
+
+DROP TABLE IF EXISTS `pessoas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pessoas` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(30) NOT NULL,
+  `profissao` varchar(20) DEFAULT '',
+  `nascimento` date DEFAULT NULL,
+  `sexo` enum('M','F') DEFAULT NULL,
+  `peso` decimal(5,2) DEFAULT NULL,
+  `altura` decimal(3,2) DEFAULT NULL,
+  `nacionalidade` varchar(20) DEFAULT 'Brasil',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pessoas`
+--
+
+LOCK TABLES `pessoas` WRITE;
+/*!40000 ALTER TABLE `pessoas` DISABLE KEYS */;
+INSERT INTO `pessoas` VALUES (1,'Jozi',NULL,'1989-06-05','F',45.20,1.58,'Brasil'),(2,'José',NULL,'1985-09-02','M',72.20,1.81,'Alemanha'),(3,'José',NULL,'1985-09-02','M',72.20,1.81,'Alemanha'),(4,'João',NULL,'1955-02-10','M',73.20,1.81,'França'),(5,'Pedro',NULL,'1995-06-05','M',59.80,1.81,'Turquia'),(6,'Maria',NULL,'1972-05-02','F',50.20,1.81,'Etiópia');
+/*!40000 ALTER TABLE `pessoas` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-11-07 21:31:10
